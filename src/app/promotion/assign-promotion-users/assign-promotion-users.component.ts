3a9cfbda0426ad5309c3d09ef2ed01a879238772
zzr6036 <zzrchirstina@gmail.com>
2018-10-11 16:04:00 +0800

    11thOct2018

--- src/app/promotion/assign-promotion-users/assign-promotion-users.component.ts
+++ src/app/promotion/assign-promotion-users/assign-promotion-users.component.ts
@@ -1,26 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { PromotionService } from "../promotion.service";
 import { PromotionView } from "../promotion-view.model";
-import { global } from '../../global';
+import { global } from "../../global";
 import { HttpModule, Http, Response } from "@angular/http";
 import { ActivatedRoute, Router } from "@angular/router";
-import * as moment from 'moment';
-import { AngularMultiSelect } from 'angular2-multiselect-dropdown/multiselect.component';
-import Swal from 'sweetalert2';
-import { assertPlatform } from '@angular/core/src/application_ref';
+import * as moment from "moment";
+import { AngularMultiSelect } from "angular2-multiselect-dropdown/multiselect.component";
+import Swal from "sweetalert2";
+import { assertPlatform } from "@angular/core/src/application_ref";
 
 @Component({
-  selector: 'app-assign-promotion-users',
-  templateUrl: './assign-promotion-users.component.html',
-  styleUrls: ['./assign-promotion-users.component.scss']
+  selector: "app-assign-promotion-users",
+  templateUrl: "./assign-promotion-users.component.html",
+  styleUrls: ["./assign-promotion-users.component.scss"]
 })
 export class AssignPromotionUsersComponent implements OnInit {
   promotionDetail: PromotionView = new PromotionView();
   promotionCodes: any;
   userInfoes: Array<any> = [];
+  filterUserInfoes: Array<any> = [];
   assignUsersPromoCode: any;
   minValue: Number;
   maxValue: Number;
+  minId: Number;
+  maxId: Number;
   assignQty: Number;
   Message: String;
   Subject: string;
@@ -29,12 +32,16 @@ export class AssignPromotionUsersComponent implements OnInit {
   selectedUsers = [];
   selectedUsersObj = [];
   selectedAll = false;
-
-  // Filter by balance
   userObject = {};
   userList: Array<any> = [];
+  // Filter by balance
 
-  constructor( public http: Http, private router: Router, private route: ActivatedRoute) { }
+
+  constructor(
+    public http: Http,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.promotionCodes = window.localStorage.getItem("assignPromoCodeAll");
@@ -52,148 +59,229 @@ export class AssignPromotionUsersComponent implements OnInit {
     this.promotionDetail.AssignOnly = assignPromoCodeAll["AssignOnly"];
     this.promotionDetail.MerchantId = assignPromoCodeAll["MerchantId"];
     this.promotionDetail.Amount = assignPromoCodeAll["Amount"];
-    this.promotionDetail.MaxRedemptPerUser = assignPromoCodeAll["MaxRedemptPerUser"];
+    this.promotionDetail.MaxRedemptPerUser =
+      assignPromoCodeAll["MaxRedemptPerUser"];
     this.promotionDetail.MaxDiscount = assignPromoCodeAll["MaxDiscount"];
     this.promotionDetail.Description = assignPromoCodeAll["Description"];
     this.promotionDetail.Description2 = assignPromoCodeAll["Description2"];
     this.promotionDetail.Status = assignPromoCodeAll["Status"];
-    this.promotionDetail.CreatedByUserId = assignPromoCodeAll["CreatedByUserId"];
+    this.promotionDetail.CreatedByUserId =
+      assignPromoCodeAll["CreatedByUserId"];
     this.promotionDetail.DeleteByUserId = assignPromoCodeAll["DeleteByUserId"];
 
     let tokenNo = localStorage.getItem("Token");
-    let getUserUrl = global.host + 'search/' + 'user/' + '?keyword=' + '&token=' + tokenNo;
-    this.http.get(getUserUrl, {}).map(res => res.json()).subscribe(data => {
-      if(data['Message']){
-        console.log(data['Message'])
-      }
-      else{
-        this.userInfoes = data;
-        this.initSelectedUser();
-      }
-    }, error => {
-      console.log(error);
-    })
+    let getUserUrl =
+      global.host + "search/" + "user/" + "?keyword=" + "&token=" + tokenNo;
+    this.http
+      .get(getUserUrl, {})
+      .map(res => res.json())
+      .subscribe(
+        data => {
+          if (data["Message"]) {
+            console.log(data["Message"]);
+          } else {
+            this.userInfoes = data;
+            this.filterUserInfoes = data;
+            this.initSelectedUser();
+          }
+        },
+        error => {
+          console.log(error);
+        }
+      );
   }
 
-  initSelectedUser(){
-    this.selectedUsers = new Array<boolean>(this.userInfoes.length).fill(false); 
+  initSelectedUser() {
+    this.selectedUsers = new Array<boolean>(this.userInfoes.length).fill(false);
   }
 
-  BalanceRange(){
-    let tokenNo = localStorage.getItem("Token")
-    let userListUrl = global.host + 'search/' + 'user/' + '?keyword=' + '&token=' + tokenNo;
-    for(var i=0; i<this.userInfoes.length; i++){
-      if(this.minValue < this.userInfoes[i]["XinDollar"] && this.maxValue > this.userInfoes[i]["XinDollar"]){
-        let seq = i;
-        this.http.get(userListUrl, {}).map(res => res.json()).subscribe(data => {
-          this.userObject = data[seq];
-          this.userList.push(this.userObject);
-          this.userObject = {};
-        })
-      }  
+  BalanceRange() {
+    let tokenNo = localStorage.getItem("Token");
+    let userListUrl = global.host + "search/" + "user/" + "?keyword=" + "&token=" + tokenNo;
+    this.filterUserInfoes = [];
+    for (var i = 0; i < this.userInfoes.length; i++) {
+      if (
+        this.minValue < this.userInfoes[i]["XinDollar"] &&
+        this.maxValue > this.userInfoes[i]["XinDollar"]
+      ) {
+        // this.userObject = this.userInfoes[i]
+        this.filterUserInfoes.push(this.userInfoes[i])
+        // this.userObject = {}
+        // this.http.get(userListUrl, {}).map(res => res.json()).subscribe(data => {
+        //     this.userObject = data[seq];
+        //     this.userList.push(this.userObject);
+        //     this.userObject = {};
+        //   });
+      }
     }
-    this.userInfoes = this.userList;
+    //this.userInfoes = this.userList;
 
-    if(this.minValue == null || this.maxValue == null){
+    if (this.minValue == null || this.maxValue == null) {
       this.ngOnInit();
     }
   }
 
-  checkAll(){
-    if(this.selectedAll){
-      this.selectedUsers = new Array<boolean>(this.userInfoes.length).fill(true); 
+  IdRangeCheck(){
+    let tokenNo = localStorage.getItem("Token");
+    let userListUrl = global.host + "search/" + "user/" + "?keyword=" + "&token=" + tokenNo;
+    this.filterUserInfoes = [];
+    for(var n=0; n<this.userInfoes.length; n++){
+      if(this.minId < this.userInfoes[n]['Id'] && this.maxId > this.userInfoes[n]['Id']){
+        // this.userObject = this.filterUserInfoes[n]
+        this.filterUserInfoes.push(this.userInfoes[n]);
+        // this.userObject = {}
+        // let seq = n;
+        // this.http.get(userListUrl, {}).map(res => res.json()).subscribe(data=>{
+        //   this.userObject = data[seq];
+        //   this.userList.push(this.userObject);
+        //   this.userInfoes = this.userList;
+        // })
+      }
+    }
+    console.log(this.userObject)
+    // this.userInfoes = this.userList;
+    // if(this.minId == null || this.maxId == null){
+    //   this.ngOnInit();
+    // }
+  }
+
+  checkAll() {
+    if (this.selectedAll) {
+      this.selectedUsers = new Array<boolean>(this.userInfoes.length).fill(
+        true
+      );
       this.selectedUsersObj = this.userInfoes;
       // console.log(this.selectedUsersObj)
-    }
-    else{
-      this.selectedUsers = new Array<boolean>(this.userInfoes.length).fill(false); 
+    } else {
+      this.selectedUsers = new Array<boolean>(this.userInfoes.length).fill(
+        false
+      );
       this.selectedUsersObj = [];
     }
   }
 
-  submit(){
+  submit() {
     //Push all selected user into assign queue
-    if(!this.selectedAll){    
+    if (!this.selectedAll) {
       let i = 0;
-      for(let isSelected of this.selectedUsers){
-        if(isSelected){
+      this.selectedUsersObj = [];
+      for (let isSelected of this.selectedUsers) {
+        if (isSelected) {
           this.selectedUsersObj.push(this.userInfoes[i]);
         }
         ++i;
-      } 
+      }
     }
-     // Assign Promotion Code to selected user
+    // Assign Promotion Code to selected user
     let tokenNo = localStorage.getItem("Token");
-    let assignPromoCodeUrl = global.host + 'promocodes/' + '?token=' + tokenNo;
-    for(var n=0; n<this.selectedUsersObj.length; n++){
-      this.assignUsersPromoCode = {
-        Id:  this.promotionDetail.Id,
-        Code: this.promotionDetail.Code,
-        Title: this.promotionDetail.Title,
-        MinUsed: this.promotionDetail.MinUsed,
-        StartTime: this.promotionDetail.StartTime,
-        EndTime: this.promotionDetail.EndTime,
-        Qty: this.promotionDetail.Qty,
-        IsSpecial: this.promotionDetail.IsSpecial,
-        IsPercent: this.promotionDetail.IsPercent,
-        IsJoint: this.promotionDetail.IsJoint,
-        AssignOnly: this.promotionDetail.AssignOnly,
-        MerchantId: this.promotionDetail.MerchantId,
-        Amount: this.promotionDetail.Amount,
-        _maxRedempt: 0,
-        MaxDiscount: this.promotionDetail.MaxDiscount,
-        Description: this.promotionDetail.Description,
-        Description2: this.promotionDetail.Description2,
-        Status: this.promotionDetail.Status,
-        Message: this.Message,
-        Subject: this.Subject,
-        CreatedByUserId: this.promotionDetail.CreatedByUserId,
-        DeleteByUserId: this.promotionDetail.DeleteByUserId,
-        UserId: this.selectedUsersObj[n]["Id"],
-      }
-      if(this.promotionDetail.IsSpecial == true){
+    let assignPromoCodeUrl = global.host + "promocodes/" + "?token=" + tokenNo;
+    this.assignUsersPromoCode = {
+      Id: this.promotionDetail.Id,
+      Code: this.promotionDetail.Code,
+      Title: this.promotionDetail.Title,
+      MinUsed: this.promotionDetail.MinUsed,
+      StartTime: this.promotionDetail.StartTime,
+      EndTime: this.promotionDetail.EndTime,
+      Qty: this.promotionDetail.Qty,
+      IsSpecial: this.promotionDetail.IsSpecial,
+      IsPercent: this.promotionDetail.IsPercent,
+      IsJoint: this.promotionDetail.IsJoint,
+      AssignOnly: this.promotionDetail.AssignOnly,
+      MerchantId: this.promotionDetail.MerchantId,
+      Amount: this.promotionDetail.Amount,
+      _maxRedempt: 0,
+      MaxDiscount: this.promotionDetail.MaxDiscount,
+      Description: this.promotionDetail.Description,
+      Description2: this.promotionDetail.Description2,
+      Status: this.promotionDetail.Status,
+      Message: this.Message,
+      Subject: this.Subject,
+      CreatedByUserId: this.promotionDetail.CreatedByUserId,
+      DeleteByUserId: this.promotionDetail.DeleteByUserId,
+      UserId: null
+    };
+    this.recursiveSubmit(0, assignPromoCodeUrl);
+    //for (var n = 0; n < this.selectedUsersObj.length; n++) {
+      // this.assignUsersPromoCode.UserId = this.selectedUsersObj[n]["Id"];
+      // if (this.promotionDetail.IsSpecial == true) {
+      //   Swal({
+      //     position: "center",
+      //     type: "info",
+      //     title: "The Special Promotion Code is not allowed to assign customer",
+      //     showConfirmButton: true
+      //   }).then(() => {
+      //     this.router.navigate(["/promotion"]);
+      //   });
+      // } else {
+        //this.assignUsersPromoCode.UserId = this.selectedUsersObj[n]["Id"];
+        // if (this.promotionDetail.Qty > this.selectedUsersObj.length) {
+        //   this.http
+        //     .post(assignPromoCodeUrl, this.assignUsersPromoCode, {})
+        //     .map(res => res.json())
+        //     .subscribe(
+        //       data => {
+        //         if (data["Message"] == undefined) {
+        //           this.router.navigate(["/promotion/"]);
+        //         } else {
+        //           console.log(data["Message"]);
+        //         }
+        //       },
+        //       error => {
+        //         console.log(error);
+        //       }
+        //     );
+        // } else {
+        //   Swal({
+        //     position: "center",
+        //     type: "warning",
+        //     title:
+        //       "-----Unable Assign to User-----(The quantity of current promotion code is insufficient)",
+        //     showConfirmButton: true
+        //   });
+        // }
+      // }
+    //}
+    // console.log(this.selectedUsersObj)
+  }
+
+  recursiveSubmit(inIndex, assignPromoCodeUrl){
+    if(inIndex < this.selectedUsersObj.length){
+      this.assignUsersPromoCode.UserId = this.selectedUsersObj[inIndex]["Id"];
+      if (this.promotionDetail.Qty > this.selectedUsersObj.length) {
+        this.http
+          .post(assignPromoCodeUrl, this.assignUsersPromoCode, {})
+          .map(res => res.json())
+          .subscribe(
+            data => {
+              if (data["Message"] == undefined) {
+                ++inIndex;
+                this.recursiveSubmit(inIndex, assignPromoCodeUrl);
+              } else {
+                console.log(data["Message"]);
+                ++inIndex;
+                this.recursiveSubmit(inIndex, assignPromoCodeUrl);
+              }
+            },
+            error => {
+              console.log(error);
+              ++inIndex;
+              this.recursiveSubmit(inIndex, assignPromoCodeUrl);
+            }
+          );
+      } else {
         Swal({
-          position: 'center',
-          type: 'info',
-          title: 'The Special Promotion Code is not allowed to assign customer',
-          showConfirmButton: true,
-        }).then(() => {
-          this.router.navigate(['/promotion']);
-        })
-      }
-      else{
-        if(this.promotionDetail.Qty > this.selectedUsersObj.length){
-          this.http.post(assignPromoCodeUrl, this.assignUsersPromoCode, {}).map(res => res.json()).subscribe(data => {
-            // console.log(data)
-            if(data["Message"] == undefined){
-              this.router.navigate(['/promotion/']);
-              // Swal({
-              //   position: 'center',
-              //   type: 'success',
-              //   title: 'Assign Successfully',
-              //   showConfirmButton: false,
-              //   timer: 1500,
-              // }).then(() => {
-              //   this.router.navigate(['/promotion/']);
-              // })
-            }
-            else{
-              console.log(data["Message"]);
-            }
-      }, error => {
-        console.log(error);
-      })
-        }
-        else {
-          Swal({
-            position:'center',
-            type:'warning',
-            title: '-----Unable Assign to User-----(The quantity of current promotion code is insufficient)',
-            showConfirmButton: true,
-          })
-        }
+          position: "center",
+          type: "warning",
+          title:
+            "-----Unable Assign to User-----(The quantity of current promotion code is insufficient)",
+          showConfirmButton: true
+        });
+        ++inIndex;
+        this.recursiveSubmit(inIndex, assignPromoCodeUrl);
       }
     }
-    // console.log(this.selectedUsersObj)
+    else{
+      this.router.navigate(["/promotion/"]);
+    }
   }
 }
